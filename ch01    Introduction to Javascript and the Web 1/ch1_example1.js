e4310e39de9006a23d8a69f8eced6e0188b0ac24
Jeffrin Jose Thalakkottoor <jeffrin@rajagiritech.edu.in>
2025-08-17 22:48:06 +0530

    beginning javascript

---
+++ ch01    Introduction to Javascript and the Web 1/ch1_example1.js
@@ -0,0 +1,7 @@
+     // document.bgColor = "red";
+        if (typeof window !== 'undefined' && typeof document !== 'undefined') {
+            // Code that uses the 'document' object;
+	        document.bgColor = "red";
+        }
+
+       
