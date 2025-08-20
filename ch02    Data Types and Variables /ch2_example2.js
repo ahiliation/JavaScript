43ff8e55cad47635928ee633e9480ea572c7e346
Jeffrin Jose Thalakkottoor <jeffrin@rajagiritech.edu.in>
2025-08-20 21:24:04 +0530

    alert -- console.log related

---
+++ ch02    Data Types and Variables /ch2_example2.js
@@ -0,0 +1,15 @@
+﻿        var string1 = "Hello";
+        var string2 = "Goodbye";
+
+        console.log(string1);
+        console.log(string2);
+
+        string2 = string1;
+
+        console.log(string1);
+        console.log(string2);
+
+        string1 = "Now for something different";
+
+        console.log(string1);
+        console.log(string2);
