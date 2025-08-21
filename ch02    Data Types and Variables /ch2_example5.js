ec135505c760ff26811ea5f191677c198e8da6f8
Jeffrin Jose Thalakkottoor <jeffrin@rajagiritech.edu.in>
2025-08-21 19:44:25 +0530

    JS prompt-sync related

---
+++ ch02    Data Types and Variables /ch2_example5.js
@@ -0,0 +1,7 @@
+﻿const prompt = require('prompt-sync')({ sigint: true }); // { sigint: true } allows Ctrl+C to exit
+var greetingString = "Hello";
+let myName = prompt('Enter your name: ');
+var concatString;
+console.log(greetingString + " " + myName);
+concatString = greetingString + " " + myName;
+console.log(concatString);
