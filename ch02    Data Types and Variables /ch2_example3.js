a391220e83c2bbea33aa2a2d97af110038e4e4be
Jeffrin Jose Thalakkottoor <jeffrin@rajagiritech.edu.in>
2025-08-21 16:30:30 +0530

    JS division related -- node

---
+++ ch02    Data Types and Variables /ch2_example3.js
@@ -0,0 +1,10 @@
+﻿        var firstNumber = 15;
+        var secondNumber = 10;
+        var answer;
+        answer = 15 / 10;
+        console.log(answer);
+
+        console.log(15 / 10);
+
+        answer = firstNumber / secondNumber;
+        console.log(answer);
