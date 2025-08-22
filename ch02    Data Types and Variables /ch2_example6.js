0425bf158268db28ce2116dc25634f48b45f51b2
Jeffrin Jose Thalakkottoor <jeffrin@rajagiritech.edu.in>
2025-08-22 19:34:22 +0530

    F -> C -- node related

---
+++ ch02    Data Types and Variables /ch2_example6.js
@@ -0,0 +1,6 @@
+﻿const prompt = require('prompt-sync')({ sigint: true }); // { sigint: true } allows Ctrl+C to exit
+        // Equation is °C = 5/9 (°F - 32).
+var degFahren = prompt("Enter the degrees in Fahrenheit:  ");
+        var degCent;
+        degCent = 5/9 * (degFahren - 32);
+console.log(degFahren + "  Fahrenheit is " + degCent + "  centigrade");
