7a85831fc00e6fced39aba92cfe28c7b80a76e94
Jeffrin Jose Thalakkottoor <jeffrin@rajagiritech.edu.in>
2025-08-22 21:36:36 +0530

    parseInt and parseFloat -- node

---
+++ ch02    Data Types and Variables /ch2_example7.js
@@ -0,0 +1,11 @@
+﻿var myString = "56.02 degrees centigrade  ";
+var myInt;
+var myFloat;
+
+console.log(myString + "is  "  + parseInt(myString, 10) + "  as an integer");
+
+myInt = parseInt(myString, 10);
+console.log(myString + "when converted to an integer equals " + myInt);
+
+myFloat = parseFloat(myString);
+console.log(myString + "when converted to a floating point number equals "+ myFloat);
