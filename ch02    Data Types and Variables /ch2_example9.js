7fe5105eca947fde014507091ccff04bff8f56f9
Jeffrin Jose Thalakkottoor <jeffrin@rajagiritech.edu.in>
2025-08-23 21:39:09 +0530

    DS array  multiD -- node  related

---
+++ ch02    Data Types and Variables /ch2_example9.js
@@ -0,0 +1,21 @@
+﻿var personnel = [];
+
+personnel[0] = [];
+personnel[0][0] = "Name0";
+personnel[0][1] = "Age0";
+personnel[0][2] = "Address0";
+
+personnel[1] = [];
+personnel[1][0] = "Name1";
+personnel[1][1] = "Age1";
+personnel[1][2] = "Address1";
+
+personnel[2] = [];
+personnel[2][0] = "Name2";
+personnel[2][1] = "Age2";
+personnel[2][2] = "Address2";
+
+console.log("Name : "    + personnel[1][0]);
+console.log("Age : "     + personnel[1][1]);
+console.log("Address :"  + personnel[1][2]);
+console.table(personnel);
