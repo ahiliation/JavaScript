7a647f6729c22cfcd0eac90d1435de31b595bffa
Jeffrin Jose Thalakkottoor <jeffrin@rajagiritech.edu.in>
2025-08-23 20:56:16 +0530

    DS array  -- node related

---
+++ ch02    Data Types and Variables /ch2_example8.js
@@ -0,0 +1,11 @@
+﻿var myArray = [];
+myArray[0] = "Jeremy";
+myArray[1] = "Paul";
+myArray[2] = "John";
+
+console.log("myArray[0] = " + myArray[0]);
+console.log("myArray[2] = " + myArray[2]); 
+console.log("myArray[1] = " + myArray[1]);
+
+myArray[1] = "Mike"; 
+console.log("myArray[1] changed to " + myArray[1]);
