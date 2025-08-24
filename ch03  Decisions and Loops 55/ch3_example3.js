cf79f1e955e226c829b01268dd898d5312e5c042
Jeffrin Jose Thalakkottoor <jeffrin@rajagiritech.edu.in>
2025-08-24 23:27:43 +0530

    switch case related

---
+++ ch03  Decisions and Loops 55/ch3_example3.js
@@ -0,0 +1,32 @@
+﻿const prompt = require('prompt-sync')({ sigint: true });
+
+var secretNumber = prompt("Pick a number between 1 and 5:", "");
+        secretNumber = parseInt(secretNumber, 10);
+
+        switch (secretNumber) {
+            case 1:
+                console.log("Too low!");
+                break;
+
+            case 2:
+                console.log("Too low!");
+                break;
+
+            case 3:
+                console.log("You guessed the secret number!");
+                break;
+
+            case 4:
+                console.log("Too high!");
+                break;
+
+            case 5:
+                console.log("Too high!");
+                break;
+
+            default:
+                console.log("You did not enter a number between 1 and 5.");
+                break;
+        }
+
+        console.log("Execution continues here");
