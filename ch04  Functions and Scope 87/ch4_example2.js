b3460e547a6e5acf5c1925b4a699b5a0ae120406
Jeffrin Jose Thalakkottoor <jeffrin@rajagiritech.edu.in>
2025-08-27 22:02:15 +0530

    Functions and scope-- node  related

---
+++ ch04  Functions and Scope 87/ch4_example2.js
@@ -0,0 +1,20 @@
+﻿        function toCentigrade(degFahren) {
+            var degCent = 5 / 9 * (degFahren - 32);
+
+            console.log(degFahren + " Fahrenheit is " +
+                           degCent + " Celsius.");
+        }
+
+        function toFahrenheit(degCent) {
+            var degFahren = 9 / 5 * degCent + 32;
+
+            console.log(degCent + " Celsius is " +
+                           degFahren + " Fahrenheit.");
+        }
+
+        function convert(converter, temperature) {
+            converter(temperature);
+        }
+
+        convert(toFahrenheit, 23);
+        convert(toCentigrade, 75);
