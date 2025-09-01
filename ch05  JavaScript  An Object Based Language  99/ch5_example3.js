447bfa924ea7f0750ead316ecab488ae33c6b737
Jeffrin Jose Thalakkottoor <jeffrin@rajagiritech.edu.in>
2025-09-01 23:30:42 +0530

    method on array  related --node

---
+++ ch05  JavaScript  An Object Based Language  99/ch5_example3.js
@@ -0,0 +1,17 @@
+﻿const prompt = require('prompt-sync')({ sigint: true });
+var myShopping = ["Eggs", "Milk", "Potatoes", "Cereal", "Banana"];
+
+var ord = prompt("Enter 1 for alphabetical order, " +
+                 "and -1 for reverse order ");
+console.log(ord);
+
+if (ord == 1) {
+    myShopping.sort();
+    console.log(myShopping.join(""));
+} else if (ord == -1) {
+    myShopping.sort();
+    myShopping.reverse();
+    console.log(myShopping.join("."));
+} else {
+    console.log("That is not a valid input");
+}
