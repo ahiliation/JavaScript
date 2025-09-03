af1433f352c5bc6261234b28cbc70e1209399ff8
Jeffrin Jose Thalakkottoor <jeffrin@rajagiritech.edu.in>
2025-09-04 00:27:57 +0530

    fixed to decimal places -- node

---
+++ ch05  JavaScript  An Object Based Language  99/ch5_example5.js
@@ -0,0 +1,13 @@
+﻿const prompt = require('prompt-sync')({ sigint: true });
+function fix(fixNumber, decimalPlaces) {
+var div = Math.pow(10, decimalPlaces);
+fixNumber = Math.round(fixNumber * div) / div;
+return fixNumber;
+}
+
+var number1 = prompt("Enter the number with decimal places you want to fix ");
+
+var number2 = prompt("How many decimal places do you want? ");
+
+console.log(number1 + " fixed to " + number2 + " decimal places is: ");
+console.log(fix(number1, number2));
