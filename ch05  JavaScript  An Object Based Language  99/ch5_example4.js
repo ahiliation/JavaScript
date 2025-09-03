be53dbe400be430c2eb632555b14ff2d20a6396a
Jeffrin Jose Thalakkottoor <jeffrin@rajagiritech.edu.in>
2025-09-03 22:36:14 +0530

    in-built math methods -- node

---
+++ ch05  JavaScript  An Object Based Language  99/ch5_example4.js
@@ -0,0 +1,10 @@
+﻿const prompt = require('prompt-sync')({ sigint: true });
+var myNumber = prompt("Enter the number to be rounded  ");
+
+console.log("The number you entered was " + myNumber);
+console.log("The rounding results for this number are -->");
+console.log("Using parseInt      = " + parseInt(myNumber, 10));
+console.log("Using Math.ceil     = " + Math.ceil(myNumber));
+console.log("Using Math.floor    = " + Math.floor(myNumber));
+console.log("Using Math.round    = " + Math.round(myNumber));
+       
