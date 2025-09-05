6c183f20debe9aee89849012c03e5c8acd67c867
Jeffrin Jose Thalakkottoor <jeffrin@rajagiritech.edu.in>
2025-09-05 22:47:30 +0530

    OOP related --node

---
+++ ch05  JavaScript  An Object Based Language  99/ch5_example8.js
@@ -0,0 +1,18 @@
+﻿function createPerson(firstName, lastName) {
+    return {
+        firstName: firstName,
+        lastName: lastName,
+        getFullName: function() {
+            return this.firstName + " " + this.lastName
+        },
+        greet: function(person) {
+            console.log("Hello, " + person.getFullName() +
+                  ". I'm " + this.getFullName());
+        }
+    };
+}
+
+var johnDoe = createPerson("John", "Doe");
+var janeDoe = createPerson("Jane", "Doe");
+
+johnDoe.greet(janeDoe);
