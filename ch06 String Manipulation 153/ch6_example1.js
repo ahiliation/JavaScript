44ab25bdc418dbaef0b11d1ea46ea90b2973eead
Jeffrin Jose Thalakkottoor <jeffrin@rajagiritech.edu.in>
2025-09-07 00:16:51 +0530

    string reverse related -- node

---
+++ ch06 String Manipulation 153/ch6_example1.js
@@ -0,0 +1,19 @@
+﻿const prompt = require('prompt-sync')({ sigint: true });
+var values = prompt("Please enter a set of comma separated values.",);
+
+function splitAndReverseText(csv) {
+    var parts = csv.split(",");
+		   //  <br>
+		 //   document.write("parts");
+    console.log(parts.reverse());
+    console.log(parts.join(","));
+    var reversedString = parts.join(",");
+    if (parts.reverse() == reversedString) {
+	console.log ("problem");
+    } else {
+	console.log("okay");
+    }
+    console.log("Final..."+reversedString);
+}
+
+splitAndReverseText(values);
