4bdb07da1f328e7136b08b26941ad74690276a25
Jeffrin Jose Thalakkottoor <jeffrin@rajagiritech.edu.in>
2025-09-08 21:03:43 +0530

    asynchronous function for pass entry and RE check of password related

---
+++ ch06 String Manipulation 153/ch6_example2.js
@@ -0,0 +1,18 @@
+﻿import { password } from '@inquirer/prompts';
+
+async function askForPassword() {
+  const secret = await password({
+    message: 'Please enter your password:',
+    mask: ' ', // This is optional, it replaces each character with an asterisk
+  });
+
+    var myRegExp = /[^a-z\d ]/i;                                                     
+    var bln = myRegExp.test(secret);
+
+    if (!bln) {                                                               
+	console.log("Your passphrase contains only valid characters");                      } else {                                                                                 console.log("Your passphrase contains one or more invalid characters");                  }   
+   console.log('Password entered:', secret);
+  // In a real application, you would use this password for authentication
+}
+
+askForPassword();
