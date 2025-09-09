86b2b97cfe3aaa196b83c36bdad55b977932279d
Jeffrin Jose Thalakkottoor <jeffrin@rajagiritech.edu.in>
2025-09-09 21:49:57 +0530

    RE to make an array and split and join methods

---
+++ ch06 String Manipulation 153/ch6_example3.js
@@ -0,0 +1,6 @@
+﻿var myListString = "apple, 0.99, banana, 0.50, peach, 0.25, orange, 0.75";
+var theRegExp = /[^a-z]+/i;
+var myFruitArray = myListString.split(theRegExp);
+
+console.log(myFruitArray);      
+console.log(myFruitArray.join(" "));
