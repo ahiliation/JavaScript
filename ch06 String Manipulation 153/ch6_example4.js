95050e3b2b449471e570bbce25fb15cb4e7fab16
Jeffrin Jose Thalakkottoor <jeffrin@rajagiritech.edu.in>
2025-09-09 22:05:41 +0530

    RE and replace method -- node

---
+++ ch06 String Manipulation 153/ch6_example4.js
@@ -0,0 +1,9 @@
+﻿var text = "He then said 'My Name is O'Connerly, yes " +
+    "that's right, O'Connerly'";
+
+console.log("Original: " + text );
+
+var myRegExp = /\B'|'\B/g;
+text = text.replace(myRegExp, '"');
+
+console.log("Corrected: " + text);
