d212f3943ab348f9780b7106d98b12cf1e41a9a0
Jeffrin Jose Thalakkottoor <jeffrin@rajagiritech.edu.in>
2025-09-11 21:35:11 +0530

    RE and replace method -- node

---
+++ ch06 String Manipulation 153/figures/ch6_figure2.js
@@ -0,0 +1,9 @@
+﻿var myString = "Paul, Paula, Pauline, paul, Paul";
+var myRegExp = /Paul/;
+
+myString = myString.replace(myRegExp, "Ringo");
+console.log(myString);
+
+/* The replace method searches a string for a value or a regular expression.
+   it returns a new string with the value(s) replaced.
+   it does not change the original string. */
