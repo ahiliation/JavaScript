d48dae753b6b5ba4e14357a10806ca65b0d17a0a
Jeffrin Jose Thalakkottoor <jeffrin@rajagiritech.edu.in>
2025-09-11 20:41:59 +0530

    RE and match and join methods -- node

---
+++ ch06 String Manipulation 153/ch6_example5.js
@@ -0,0 +1,8 @@
+﻿var html = "<h2>Hello World!</h2>" +
+    "<p>We love JavaScript!</p>";
+
+var regex = /<[^>\r\n]+>|[^<>\r\n]+/g;
+var results = html.match(regex);
+
+console.log(results.join("\r\n"));
+    
