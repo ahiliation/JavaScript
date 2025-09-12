bc5cc15ea549e153070bc74c4d5a814b3001d1f9
Jeffrin Jose Thalakkottoor <jeffrin@rajagiritech.edu.in>
2025-09-12 22:28:30 +0530

    RE and replace method and doc --node

--- ch06 String Manipulation 153/figures/ch6_figure4.js
+++ ch06 String Manipulation 153/figures/ch6_figure4.js
@@ -3,3 +3,8 @@
 var myRegExp = /\b/g;
 myString = myString.replace(myRegExp, "|");
 console.log(myString);
+
+/* \b: This is the word boundary metacharacter.
+   It is a zero-length assertion, meaning it matches a
+   position in the string, not a character. A position
+   is considered a word boundary if it meets one of three criteria: */
