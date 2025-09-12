0f9d7a1f74ad9097dd1af665c55431e77c768e1f
Jeffrin Jose Thalakkottoor <jeffrin@rajagiritech.edu.in>
2025-09-12 22:50:59 +0530

    RE and replace method and doc --node

---
+++ ch06 String Manipulation 153/figures/ch6_figure5.js
@@ -0,0 +1,10 @@
+﻿var myString = "Hello world!, let's look at boundaries said 007.";
+
+var myRegExp = /\B/g;
+myString = myString.replace(myRegExp, "|");
+console.log(myString);
+
+/* The regular expression /\B/g in JavaScript matches a position
+   that is not a word boundary, applying this check globally
+   throughout the string. It is the logical opposite of the \b
+   (word boundary) metacharacter. */
