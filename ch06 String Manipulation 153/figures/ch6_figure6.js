bcdec62bb9985a63355e66308a967c60c76ab7b1
Jeffrin Jose Thalakkottoor <jeffrin@rajagiritech.edu.in>
2025-09-13 21:42:51 +0530

    RE and replace method and \W/gi -- node

---
+++ ch06 String Manipulation 153/figures/ch6_figure6.js
@@ -0,0 +1,9 @@
+﻿var myString = "Paul, Paula, Pauline, paul, Paul";
+var myRegExp = /Paul\W/gi;
+
+myString = myString.replace(myRegExp, "Ringo");
+console.log(myString);
+
+/* The JavaScript regular expression /\W/gi is a pattern used
+   to find and match all non-word characters in a string, regardless
+   of their case. The pattern is broken down into three components: */ 
