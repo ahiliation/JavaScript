8e26bc79f225fcaf5a1ca6294eccaafa766afc4f
Jeffrin Jose Thalakkottoor <jeffrin@rajagiritech.edu.in>
2025-09-13 22:46:05 +0530

    RE and replace method and /\b(VB)?(Java)?Script\b/gi  --node

---
+++ ch06 String Manipulation 153/figures/ch6_figure8.js
@@ -0,0 +1,9 @@
+﻿var myString = "JavaScript, VBScript and PHP";
+var myRegExp = /\b(VB)?(Java)?Script\b/gi;
+
+myString = myString.replace(myRegExp, "xxxx");
+console.log(myString);
+
+/* The regular expression /\\b(VB)?(Java)?Script\\b/gi is used
+   to find instances of "JavaScript," "VBScript," "JScript,"
+   or simply "Script" as whole words within a text, regardless of case. */
