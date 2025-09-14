2b1299ca5274e97e2fa8b76aef3dbe0e958d7e85
Jeffrin Jose Thalakkottoor <jeffrin@rajagiritech.edu.in>
2025-09-15 01:38:20 +0530

    different times related tolocal time --node

---
+++ ch07  Date, Time, and Timers 193/ch7_example1.js
@@ -0,0 +1,11 @@
+﻿var localTime = new Date();
+
+var html = "UTC Time is " + localTime.toUTCString();
+html += "Local Time is " + localTime.toLocaleString();
+html += "Time Zone Offset is " + localTime.getTimezoneOffset();
+html += "Using toLocalTimeString() gives: " + localTime.toLocaleTimeString();
+html += "Using toTimeString() gives: " + localTime.toTimeString();
+html += "Using toLocaleDateString() gives: " + localTime.toLocaleDateString();
+html += "Using toDateString() gives: : " + localTime.toDateString();
+
+console.log(html);
