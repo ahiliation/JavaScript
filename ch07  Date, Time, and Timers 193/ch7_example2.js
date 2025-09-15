ee040a8db99a70b10bfa4e23700c6e1b4115b983
Jeffrin Jose Thalakkottoor <jeffrin@rajagiritech.edu.in>
2025-09-15 22:13:59 +0530

    setting date and time related  --node

---
+++ ch07  Date, Time, and Timers 193/ch7_example2.js
@@ -0,0 +1,9 @@
+﻿var myDate = new Date();
+myDate.setUTCHours(12);
+myDate.setUTCMinutes(0);
+myDate.setUTCSeconds(0);
+
+var html = myDate.toUTCString() + "\n";
+html += myDate.toLocaleString() + "\n";
+
+console.log(html);
