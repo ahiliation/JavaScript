0227ffb64e921dbadcecfdf44fa743a277103367
Jeffrin Jose Thalakkottoor <jeffrin@rajagiritech.edu.in>
2025-09-16 22:01:46 +0530

    Geolocation finder trial , but failed -- node

---
+++ ch08  Programming the Browser 205/ch8_example1.js
@@ -0,0 +1,15 @@
+﻿function geoSuccess(position) {
+    var coords = position.coords;
+    var latitude = coords.latitude;
+    var longitude = coords.longitude;
+
+    var message = "You're at " + latitude + ", " + longitude
+
+    console.log(message);
+}
+
+function geoError(errorObj) {
+    console.log(errorObj.message);
+}
+
+navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
