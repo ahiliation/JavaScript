0d80720122fbeec15093a37951236f8bc936d547
Jeffrin Jose Thalakkottoor <jeffrin@rajagiritech.edu.in>
2025-12-02 21:54:10 +0530

    concatenate arrays

---
+++ Chapter2  Arrays/chap2_4.js
@@ -0,0 +1,6 @@
+var cis = ["Mike","Clayton","Terrill","Danny","Jennifer"];
+var dmp = ["Raymond","Cynthia","Bryan"];
+var it = cis.concat(dmp);
+console.log(it); 
+it = dmp.concat(cis);
+console.log(it);
