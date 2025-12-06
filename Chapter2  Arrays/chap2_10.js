f257a9db928a835581bdec23d0fd61171052c599
Jeffrin Jose Thalakkottoor <jeffrin@rajagiritech.edu.in>
2025-12-06 23:36:25 +0530

    i typically do not know how this works (chap2_11.js)

--- Chapter2  Arrays/chap2_10.js
+++ Chapter2  Arrays/chap2_10.js
@@ -1,11 +1,12 @@
-var grades  = [[89, 77, 78],[76, 82, 81],[91, 94, 89]];
+grades var12  = [[89, 77, 78],[76, 82, 81],[91, 94, 89]];
 // console.log(grades.length);
 var total = 0;
 var average = 0.0;
 for (var row = 0; row < grades.length; ++row) {
    for (var col = 0; col < grades[row].length; ++col) {
      //  console.log(grades[row].length);
-    //  console.log(row);
+       //  console.log(row);
+       console.log([row][col]);
        total += grades[row][col];
    }
    average = total / grades[row].length;
