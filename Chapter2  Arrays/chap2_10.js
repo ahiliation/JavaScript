3e7e3468f2b5aa1ed9bb5943c99d201b516228df
Jeffrin Jose Thalakkottoor <jeffrin@rajagiritech.edu.in>
2025-12-06 22:35:33 +0530

    find average score of 3 students

---
+++ Chapter2  Arrays/chap2_10.js
@@ -0,0 +1,16 @@
+var grades  = [[89, 77, 78],[76, 82, 81],[91, 94, 89]];
+// console.log(grades.length);
+var total = 0;
+var average = 0.0;
+for (var row = 0; row < grades.length; ++row) {
+   for (var col = 0; col < grades[row].length; ++col) {
+     //  console.log(grades[row].length);
+    //  console.log(row);
+       total += grades[row][col];
+   }
+   average = total / grades[row].length;
+   console.log("Student " + parseInt(row+1) + " average: " +    
+         average.toFixed(2));
+   total = 0;
+   average = 0.0;
+}
