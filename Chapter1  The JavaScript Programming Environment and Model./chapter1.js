24083e85e2e3428ca8245dd036d1829b064bc8da
Jeffrin Jose Thalakkottoor <jeffrin@rajagiritech.edu.in>
2026-02-26 18:42:58 +0530

    debug to learn

--- Chapter1  The JavaScript Programming Environment and Model./chapter1.js
+++ Chapter1  The JavaScript Programming Environment and Model./chapter1.js
@@ -1,19 +1,21 @@
-/*var david = {
+var david = {
    first_name: "David", 
    last_name: "Durr"
 };
 
 var student1 = {
-   id: "1234",
+   id: "u12131",
    courses: ["Programming I", "English II", "Algebra"],
    advised: false
 };
 
 console.log(david.last_name); // displays "Durr"
-console.log(student1["id"]); // displays "1234"
-console.log(student1.courses[1]); displays "English II"
+// console.log(typeof david.last_name);
+console.log(student1["id"]); // displays "u12131"
+console.log(student1.courses[1]); // displays "English II"
 student1.advised = true;
-david.age = 50;*/
+david.age = 50;
+// console.log(typeof david.age);
 function Person(first, middle, last) {
    this.first = first;
    this.middle = middle;
@@ -21,11 +23,13 @@ function Person(first, middle, last) {
    this.initials = initials;
 }
 
+// console.log(typeof Person);
 function initials() {
    return this.first[0] + this.middle[0] + this.last[0];
 }
 
 var aPerson = new Person("John","Quincy","Public");
+// console.log(typeof aPerson);
 console.log("First name: " + aPerson.first);
 console.log("Middle name: " + aPerson.middle);
 console.log("Last name: " + aPerson.last);
