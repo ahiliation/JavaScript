f2adca14a4ffeea9592b064d75a68f3658350a6b
Jeffrin Jose Thalakkottoor <jeffrin@rajagiritech.edu.in>
2026-03-01 20:48:10 +0530

    writing code for the condition of - name not found

--- Chapter2  Arrays/chap2_3.js
+++ Chapter2  Arrays/chap2_3.js
@@ -1,6 +1,13 @@
 var names = ["David","Mike","Cynthia","Raymond","Clayton","Mike","Jennifer"];
-var name = "Mike";
+var name = "Davi";
 var firstPos = names.indexOf(name);
+if (firstPos = -1) {
+    console.log("Given name not found in the array");
+}
+else
+{
+// console.log(typeof firstPos);
 console.log("First found " + name + " at position " + firstPos);
 var lastPos = names.lastIndexOf(name);
 console.log("Last found " + name + " at position " + lastPos);
+    }
